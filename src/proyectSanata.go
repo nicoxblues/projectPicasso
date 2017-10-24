3eda0e6beea3503c4a3f8f64dad417448c3b47dc
nicoBlues <nicoxblues86@gmail.com>
2017-10-24 00:25:21 -0300

    UI
    se logra hacer que el user disponga de una posicion X e Y para la foto

--- src/proyectSanata.go
+++ src/proyectSanata.go
@@ -13,21 +13,17 @@ import (
 
 	"github.com/gorilla/websocket"
 	_ "golang.org/x/net/websocket"
-	"gopkg.in/antage/eventsource.v1"
 
+	_ "encoding/json"
 	_ "time"
-	 _"encoding/json"
+	"strconv"
 )
 
+type deviceConfiguration struct {
+	ResolutionHeight int
+	ResolutionWidth  int
 
-
-type deviceConfiguration struct{
-
-	ResolutionHeight string  `json:"height"`
-	ResolutionWidth string   `json:"width"`
-
-	XCoordinate int  `json:"xValue"`
-	YCoordinate int `json:"yValue"`
+	Coordinate image.Point
 
 }
 
@@ -36,40 +32,31 @@ type Client struct {
 	clientID    string
 	config      deviceConfiguration
 	isConnected bool
-	send   chan []byte
-	prossPic   chan image.Image
-	sendPic chan []byte
-	graphicID int
-
-
+	send        chan []byte
+	prossPic    chan image.Image
+	sendPic     chan []byte
+	graphicID   int
 }
 
-
 type ClientManager struct {
-	clients    map[*Client]bool
-	picture    chan image.Image
-	broadcast  chan []byte
-	register   chan *Client
-	unregister chan *Client
+	clients      map[*Client]bool
+	picture      chan image.Image
+	broadcast    chan []byte
+	register     chan *Client
+	unregister   chan *Client
 	picBroadcast chan []byte
 }
 
-
 var manager = ClientManager{
-	picture:    make(chan image.Image),
-	broadcast:  make(chan []byte),
-	register:   make(chan *Client),
-	unregister: make(chan *Client),
-	clients:    make(map[*Client]bool),
+	picture:      make(chan image.Image),
+	broadcast:    make(chan []byte),
+	register:     make(chan *Client),
+	unregister:   make(chan *Client),
+	clients:      make(map[*Client]bool),
 	picBroadcast: make(chan []byte),
-
 }
 
-
-var clientConn = make(map[*websocket.Conn]Client) //make(map[*websocket.Conn]bool) // connected clientConn
-
-
-
+//var clientConn = make(map[*websocket.Conn]Client) //make(map[*websocket.Conn]bool) // connected clientConn
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -77,35 +64,52 @@ var upgrader = websocket.Upgrader{
 }
 
 
+func (manager *ClientManager) send(message []byte ) {
+	for conn := range manager.clients {
+			conn.send <- message
+
+	}
+}
+
 func (manager *ClientManager) start() {
 	for {
 		select {
-		case cliReg := <-manager.register:
-			manager.clients[cliReg] = true
-			//jsonMessage, _ := json.Marshal(&Message{Content: "/A new socket has connected."})
-			//manager.send(jsonMessage, cliReg)
-		case clieUnReg := <-manager.unregister:
-			if _, ok := manager.clients[clieUnReg]; ok {
-				close(clieUnReg .send)
-				delete(manager.clients, clieUnReg)
-		//		jsonMessage, _ := json.Marshal(&Message{Content: "/A socket has disconnected."})
-		//		manager.send(jsonMessage, cliReg)
-			}
-		case pic := <-manager.picture:
-			for client := range manager.clients {
-				select {
-				case client.prossPic <- pic:
-				default:
-					close(client.send)
-					delete(manager.clients, client)
+			case cliReg := <-manager.register:
+				manager.clients[cliReg] = true
+				//jsonMessage, _ := json.Marshal(&Message{Content: "/A new socket has connected."})
+				//manager.send(jsonMessage, cliReg)
+			case clieUnReg := <-manager.unregister:
+				if _, ok := manager.clients[clieUnReg]; ok {
+					close(clieUnReg.send)
+					delete(manager.clients, clieUnReg)
+					//		jsonMessage, _ := json.Marshal(&Message{Content: "/A socket has disconnected."})
+					//		manager.send(jsonMessage, cliReg)
 				}
-			}
-			for clientToSend := range manager.clients{
+			case pic := <-manager.picture:
+				for client := range manager.clients {
+					client.prossPic <- pic
 
-			}
+						/*default:
+						close(client.send)
+						delete(manager.clients, client)*/
+				}
 
+			/*case message := <-manager.picBroadcast:
+				for cli := range manager.clients {
+					select {
+						case cli.send <- message:
 
+						default:
+							close(cli.send)
+							delete(manager.clients, cli)
+
+					}
+				}*/
 		}
+			//for clientToSend := range manager.clients{
+
+			//}
+
 	}
 }
 
@@ -123,11 +127,17 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
 	}
 
-	client := &Client{clientID:"test",
+	queryValues := r.URL.Query()
 
-	config:deviceConfiguration{"1600","900",500,900},
 
-	socket:conn,isConnected:true,graphicID:1}
+
+	width , _ := strconv.Atoi(queryValues.Get("width"))
+	height , _ := strconv.Atoi(queryValues.Get("height"))
+	coorX , _ := strconv.Atoi(queryValues.Get("coordenadasX"))
+	coorY , _ := strconv.Atoi(queryValues.Get("coordenadasY"))
+	client := &Client{clientID: "test",
+					config: deviceConfiguration{height, width, image.Point{coorX,coorY} },
+					socket: conn, isConnected: true, graphicID: 1, prossPic:make(chan image.Image), send:make(chan []byte)}
 
 	manager.register <- client
 
@@ -135,9 +145,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	go client.read()
 	go client.write()
 
-
 }
-
 
 
 func (c *Client) processPic() {
@@ -148,23 +156,19 @@ func (c *Client) processPic() {
 
 	for {
 		select {
-		case picture, ok := <-c.prossPic:
-			if !ok {
-				c.socket.WriteMessage(websocket.CloseMessage, []byte{})
-				return
+			case picture, ok := <-c.prossPic:
+				if !ok {
+					c.socket.WriteMessage(websocket.CloseMessage, []byte{})
+					return
+				}
+
+				//manager.picBroadcast <- c.getByteCastPicture(&picture)
+				 c.send <- c.getByteCastPicture(&picture)
+				//c.socket.WriteMessage(websocket.TextMessage, c.getByteCastPicture(picture))
 			}
-
-			c.getByteCastPicture(picture)
-
-
-			//c.socket.WriteMessage(websocket.TextMessage, c.getByteCastPicture(picture))
-		}
 	}
 
-
-
 }
-
 
 func (c *Client) write() {
 
@@ -174,45 +178,62 @@ func (c *Client) write() {
 
 	for {
 		select {
-		case _, ok := <-c.send:
+		case mesagge, ok := <-c.send:
 			if !ok {
 				c.socket.WriteMessage(websocket.CloseMessage, []byte{})
 				return
 			}
 
-
-
-			//c.socket.WriteMessage(websocket.TextMessage, c.getByteCastPicture(picture))
+			c.socket.WriteMessage(websocket.TextMessage, mesagge)
 		}
 	}
 
-
-	
 }
 
 func (c *Client) read() {
-	
-}
-
-func (c *Client) getByteCastPicture(originalPic image.Image) []byte{
-
-
-	return  []byte(getEncodeImage(c.getChunkImageForClient(originalPic)))
 
 }
 
-func (c *Client) getChunkImageForClient(image image.Image) (image.Image) {
+func (c *Client) getByteCastPicture(originalPic *image.Image) []byte {
 
+	return []byte(getEncodeImage(c.getChunkImageForClient(originalPic)))
 
+}
 
+func (c *Client) getChunkImageForClient(originImage *image.Image) image.Image {
 
-	return nil
+	fimg, err := os.Open("img/imagen.jpg")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", "error to load file ", err)
+
+	}
+
+	defer fimg.Close()
+
+	imgReal, _, err := image.Decode(fimg)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", "error to decode file "+fimg.Name(), err)
+
+	}
+
+	//bounds := imgReal.Bounds()
+	chunkWidth := c.config.ResolutionWidth
+	chunkHeight := c.config.ResolutionHeight
+	fmt.Printf("%v\n%v\n", chunkHeight, chunkWidth)
+
+	rec := imgReal.Bounds()
+	m0 := image.NewRGBA(image.Rect(0, 0,chunkWidth ,chunkHeight))
+	draw.Draw(m0, image.Rect(0, 0, rec.Max.X,rec.Max.Y), imgReal, c.config.Coordinate, draw.Src)
+	//m1 := m0.SubImage(image.Rect(0, 0, chunkWidth, chunkHeight)).(*image.RGBA)
+
+	return m0
 }
 
 func getEncodeImage(image image.Image) string {
 
 	buffer := new(bytes.Buffer)
-
 
 	if err := jpeg.Encode(buffer, image, nil); err != nil {
 		log.Println("unable to encode image.")
@@ -225,7 +246,7 @@ func getEncodeImage(image image.Image) string {
 
 }
 
-func writeImage(w http.ResponseWriter) {
+func loadImg(w http.ResponseWriter, r *http.Request) {
 
 	buffer := new(bytes.Buffer)
 	fimg, _ := os.Open("img/imagen.jpg")
@@ -234,11 +255,7 @@ func writeImage(w http.ResponseWriter) {
 		log.Println("unable to encode image.")
 	}
 
-	//w.Header().Set("Content-Type", "text/xml")
-	w.Header().Set("Content-Type", "text/event-stream")
-	if _, err := w.Write(buffer.Bytes()); err != nil {
-		log.Println("unable to write image.")
-	}
+	manager.picture <- imgReal
 
 }
 
@@ -249,61 +266,13 @@ func main() {
 
 	http.HandleFunc("/ws", wsHandler)
 
-	es := eventsource.New(nil, nil)
-	defer es.Close()
-	http.Handle("/events", es)
+	http.HandleFunc("/loadImage", loadImg)
 
-	/*	go func() {
-			for {
-				e := string(getEncodeImage())
-				es.SendEventMessage(e, "", "")
-				log.Printf("Hello has been sent (consumers: %d)", es.ConsumersCount())
-				time.Sleep(10 * time.Second)
-			}
-		}()
-
-	*/
+	go manager.start()
 
 	err := http.ListenAndServe(":8080", nil) // set listen port
 	if err == nil {
 		log.Fatal(http.ListenAndServe(":8080", nil))
 	}
-
-}
-
-func chunkImage() {
-
-	fimg, err := os.Open("img/imagen.jpg")
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %v\n", "error to load file ", err)
-
-	}
-
-	defer fimg.Close()
-	//  imgConfig, _, err := image.DecodeConfig(fimg)
-	imgReal, _, err := image.Decode(fimg)
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %v\n", "error to decode file "+fimg.Name(), err)
-
-	}
-
-	bounds := imgReal.Bounds()
-	chunkWidth := bounds.Max.X / 3 // determines the chunk width and height
-	chunkHeight := bounds.Max.Y / 1
-	fmt.Printf("%v\n%v\n", chunkHeight, chunkWidth)
-
-	m0 := image.NewRGBA(image.Rect(0, 0, chunkWidth, chunkHeight))
-	draw.Draw(m0, image.Rect(0, 0, chunkWidth*2, chunkHeight), imgReal, image.Point{chunkWidth - (chunkWidth - 413), 0}, draw.Src)
-	m1 := m0.SubImage(image.Rect(0, 0, 952, 1074)).(*image.RGBA)
-
-	toimg, _ := os.Create("img/new2.jpg")
-	defer toimg.Close()
-
-	jpeg.Encode(toimg, m1, &jpeg.Options{jpeg.DefaultQuality})
-
-	//  fmt.Printf("%v\n%v\n",chunkWidth,chunkHeight)
-	// fmt.Printf("%v\n",m1)
 
 }
